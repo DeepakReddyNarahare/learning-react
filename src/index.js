d5df15ec9df9af4d8979db09e3679c85a289813f
narahare <deepak@thedigitalacademy.co.za>
2017-09-14 22:19:43 +0200

    Components & props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-setInterval(function () {
+function Cartoon(props) { //props as properties defined by rendering types
+  return <h1>Hello, {props.name} on {props.show}</h1>
+}
+
+
+//es6 Component
+// class Cartoon extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>
+//   }
+// }
+
+function Show() {
+    return <div>
+            <Cartoon name='Pikachu' show='Pokemon'/>
+            <Cartoon name='Jasmine' show='Alladin'/>
+           </div>
+}
   ReactDOM.render(
-    <h2>Time now: {new Date().toLocaleTimeString()}</h2>,
+    <Show />,
     document.getElementById('root')
   );
-}, 1000);
