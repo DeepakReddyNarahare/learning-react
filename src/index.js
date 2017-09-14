ecfcefd91b76f9a4ad354de0449dbee1d1b921df
narahare <deepak@thedigitalacademy.co.za>
2017-09-14 22:43:03 +0200

    States & LifeCycles

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Cartoon(props) { //props as properties defined by rendering types
-  return <h1>Hello, {props.name} on {props.show}</h1>
+class Clock extends React.Component{
+
+  constructor(props){
+    super(props);
+    //how to use the state
+    this.state = {
+      date: new Date()
+    }
+  }
+
+//life cycle of the components
+  componentDidMount(){//when loads
+    this.timer = setInterval(() =>this.start(),1000);
+  }
+
+  componentWillUnmount(){//remove from dom
+    clearInterval(this.timer);
+  }
+
+  start(){
+    //update the state
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return <h1>Time is: {this.state.date.toLocaleTimeString()}</h1>
+  }
 }
 
-
-//es6 Component
-// class Cartoon extends React.Component {
-//   render() {
-//     return <h1>Hello, {this.props.name}</h1>
-//   }
-// }
-
-function Show() {
-    return <div>
-            <Cartoon name='Pikachu' show='Pokemon'/>
-            <Cartoon name='Jasmine' show='Alladin'/>
-           </div>
-}
-  ReactDOM.render(
-    <Show />,
-    document.getElementById('root')
-  );
+    ReactDOM.render(
+      <Clock />,
+      document.getElementById('root')
+    );
